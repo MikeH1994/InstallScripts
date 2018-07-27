a5fddaec7047f7dee28eb5480a33c8007c58994d
Michael Hayes <Michael.hayes.2013@live.rhul.ac.uk>
2018-07-27 10:14:10 +0100

    Create installOpenCV.sh

---
+++ OpenCV/installOpenCV.sh
@@ -0,0 +1,64 @@
+#Pilfered from https://www.learnopencv.com/install-opencv3-on-ubuntu/
+
+#Update packages
+sudo apt-get update
+sudo apt-get upgrade
+#Install dependencies
+sudo apt-get remove x264 libx264-dev
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get install git gfortran
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+sudo apt-get install libtiff5-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get install libxine2-dev libv4l-dev
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install libvorbis-dev libxvidcore-dev
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install x264 v4l-utils
+sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+#install python libraries
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo -H pip2 install -U pip numpy
+sudo -H pip3 install -U pip numpy
+sudo pip2 install virtualenv virtualenvwrapper
+sudo pip3 install virtualenv virtualenvwrapper
+#install python2 libs
+sudo pip2 install numpy scipy matplotlib scikit-image scikit-learn ipython
+sudo pip3 install numpy scipy matplotlib scikit-image scikit-learn ipython
+#download opencv from github
+cd ~
+git clone https://github.com/opencv/opencv.git
+cd opencv 
+git checkout 3.4.1 
+cd ..
+#download opencv_contrib from github
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.4.1
+cd ..
+#create build directory
+cd opencv
+mkdir build
+cd build
+#run CMake
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_C_EXAMPLES=ON \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D WITH_TBB=ON \
+      -D WITH_V4L=ON \
+      -D WITH_QT=ON \
+      -D WITH_OPENGL=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D BUILD_EXAMPLES=ON ..
+#compile and install
+make -j4
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+
